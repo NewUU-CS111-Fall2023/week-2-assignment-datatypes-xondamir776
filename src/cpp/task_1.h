78d3b6a688df4ec8348fd415da4220c305e65fe4
xondamir-zokirjonov-1500562 <127468373+xondamir776@users.noreply.github.com>
2023-10-25 18:57:27 +0500

    Update task_1.h

--- src/cpp/task_1.h
+++ src/cpp/task_1.h
@@ -3,3 +3,23 @@
  * Date:
  * Name:
  */
+#include <iostream>
+using namespace std;
+
+class Problem1 {
+private:
+    unsigned int year;
+public:
+
+    void check(){
+        cout << "Enter year: ";
+        cin >> year;
+        if (year%4==0){
+            cout << "Leap year: " << endl;
+        }
+        else{
+            cout << "It's not leap year: " << endl;
+        }
+    }
+
+};
